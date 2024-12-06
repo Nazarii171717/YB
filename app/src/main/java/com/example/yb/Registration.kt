3ffb96475bd519ca52666724848b648a6bfe3ec5
Nazarii171717 <nazarzavgar@gmail.com>
2024-12-06 01:18:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/yb/Registration.kt
@@ -0,0 +1,41 @@
+package com.example.yb
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registration)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val dobTextView: TextView = findViewById(R.id.dob)
+
+        dobTextView.setOnClickListener {
+            // Get current date
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            // Show DatePickerDialog
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val formattedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                dobTextView.text = formattedDate
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+    }
+}
